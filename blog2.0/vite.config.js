60e133bc806d1b2ab1c77becf37e10f90c3bdda1
Hayun Jung <Yunjun505@gmail.com>
2026-01-04 14:21:48 -0500

    edit vite.config

--- blog2.0/vite.config.js
+++ blog2.0/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'blog_2.0',
+  base:'/blog_2.0/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',
